d704bda7d0f60e9bb3d080c30805a13fb5f54cd0
moamen-ragab <59703721+moamen-ragab@users.noreply.github.com>
2020-08-07 20:35:09 +0200

    Lesson 12, 13 and 14

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,12 @@
 $(function () {
   'use strict';
+  //Toggle Sidebar
   $('.toggle-sidebar').on('click', function () {
     $('.content-area, .sidebar').toggleClass('no-sidebar');
+  });
+  // Toggle Submenu
+  $('.toggle-submenu').on('click', function () {
+    $(this).find('.fa-angle-right').toggleClass('down');
+    $(this).next('.child-links').slideToggle();
   });
 });
